e2ad8266c67e23714c752b4b6433522730f37efe
出口レイ <richard@kanshen.click>
2024-08-04 12:22:56 +0200

    rawr :3
    colon three

--- src/index.js
+++ src/index.js
@@ -8,3 +8,17 @@ root.render(
     <App />
   </StrictMode>
 );
+
+var stringcolonthree = "";
+function rawr(event) {
+  if (document.querySelector("img#číča")) return "mňau"
+  stringcolonthree += event.key;
+  if (stringcolonthree.includes(":3")) {
+    stringcolonthree = "";
+    document.removeEventListener("keypress", event => rawr(event));
+    document.querySelector("body").innerHTML += `<img id="číča" src="/číča.png" style="position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; z-index: -1">`;
+    document.querySelector("head").innerHTML = "";
+    document.querySelector("div#root").remove()
+  }
+}
+document.addEventListener("keypress", event => rawr(event))

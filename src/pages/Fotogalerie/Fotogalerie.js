9fc928242742ac05dc72640d70445df7c6a4ac25
出口レイ <richard@kanshen.click>
2024-10-22 21:18:26 +0200

    Update Fotogalerie.js

--- src/pages/Fotogalerie/Fotogalerie.js
+++ src/pages/Fotogalerie/Fotogalerie.js
@@ -1,9 +1,7 @@
-import "./Fotogalerie.css"
 import "./Fotogalerie.css";
 import Title from "../../components/Title/Title.js";
 
 export default function Fotogalerie() {
-    return (<div id="galleryContent"><h1>Sisinina Galerie Slávy</h1><h3>Kaming sůn :3</h3></div>)
     return (
         <div id="galleryContent">
             <Title>Sisinina Galerie Slávy</Title>

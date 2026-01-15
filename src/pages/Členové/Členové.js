553d6dfe5fafb149c938c475796690d6b218c228
NetoCZ <login.jan27@seznam.cz>
2026-01-15 14:04:38 +0100

    členové

--- src/pages/Členové/Členové.js
+++ src/pages/Členové/Členové.js
@@ -107,7 +107,9 @@ export default function Členové() {
                 <meta name="twitter:description" content="ČSA je recesistická politická strana, v tento moment chystající se vzniknout a poté začít nabírat členy. Více než strana je ČSA internetová mikrokomunita, kde každý člověk náhodně z internetu může přispět svým dílem." />
                 <meta name="twitter:image" content={`${process.env.PUBLIC_URL}/assets/banner.png`} />
             </Helmet>
+            <div>
             <Title>Členové</Title>
+            </div>
             <div id="členové">
                 {members.map((member, index) => (
                     <Člen key={index} {...member} />

582f053783cf7cba2a2b8b855027ab92ca7c095c
NetoCZ <160736886+NetoCZ@users.noreply.github.com>
2026-01-25 22:40:12 +0100

    rozpracovane(jen pro me, nemergovat nebo tak)

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -29,7 +29,7 @@ export default function Footer() {
                 <span>Tento web je open-source! Více najdete na <Link to="source">našem GitHubu</Link>  :3</span>
             </div>
             <div className="text">
-                <p>zadavatel: ČSA/zpracovatel: Richard Kužela, Jan Peška</p>
+                <p>zadavatel:ČSA/zpracovatel:Richard Kužela, Jan Peška</p>
             </div>
             <img alt='Logo České Strany Asociálů' src={logoWhite} id='footerLogo' draggable="false" />
         </div>
